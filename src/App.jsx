6531d3ff8e1f4947a9e8aa446eb1901a96685877
kennyg37 <k.ganza@alustudent.com>
2025-03-28 12:26:01 +0200

    setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/Button'
+
+const App = () => {
+  return (
+    <>
+    <div>Application initiation</div>
+    <Button/>
+    </>
+
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+      <a href='https://www.google.com' target='_blank' className=' text-green-900 p-3 ml-3'>Click <span className="">me</span></a>
+      <button className=' bg-red-700 p-5 border-2'>Button</button>
+    </div>
+    
+  )
+}
+
+export default Button
